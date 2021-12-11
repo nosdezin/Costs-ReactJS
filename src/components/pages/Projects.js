b93c85b84fafc55364b1ee1d3cfeb25e29ab923c
Nosdezin <legendfire43@gmail.com>
2021-12-11 11:19:30 -0300

    Nosde

--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -1,5 +1,28 @@
-function Projects(){
-    return (<h1>Projetos</h1>)
+import { useLocation } from "react-router-dom";
+import Container from "../layout/Container.js";
+import styles from "./Projects.module.css";
+import Message from "../layout/Message.js";
+import LinkButton from "../layout/LinkButton.js";
+
+function Projects() {
+  const location = useLocation();
+  let message = "";
+  if (location.state) {
+    message = location.state.message;
+  }
+
+  return (
+    <div className={styles.project_container}>
+      <div className={styles.title_container}>
+        <h1>Meus Projetos</h1>
+        <LinkButton to="/newproject" text="Criar Projeto" />
+      </div>
+      {message && <Message type="sucess" msg={message} />}
+      <Container customClass="start">
+        <p>Projetos...</p>
+      </Container>
+    </div>
+  );
 }
 
-export default Projects
+export default Projects;
